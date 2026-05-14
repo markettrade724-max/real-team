219ce84ebd1e224d72bb27c560cf446f0b71439e
markettrade724-max <markettrade724@gmail.com>
2026-05-14 12:42:17 +0200

    Add Weapon class with shooting functionality
    Implement shooting mechanics with bullet instantiation and recoil.

---
+++ godot-projects/genesis-3d/Weapon.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+@export var bullet_scene: PackedScene
+@export var fire_rate = 0.1
+@export var recoil = 0.1
+@export var muzzle_point: Node3D
+
+var can_fire = true
+
+func shoot():
+    if not can_fire or not bullet_scene or not muzzle_point:
+        return
+    can_fire = false
+    
+    rotation.x += randf_range(-recoil, recoil)
+    rotation.y += randf_range(-recoil, recoil)
+    
+    var bullet = bullet_scene.instantiate()
+    get_tree().root.add_child(bullet)
+    bullet.global_transform = muzzle_point.global_transform
+    
+    if bullet.has_method("launch"):
+        var direction = -muzzle_point.global_transform.basis.z
+        bullet.launch(direction * 20.0)
+    
+    await get_tree().create_timer(fire_rate).timeout
+    can_fire = true
