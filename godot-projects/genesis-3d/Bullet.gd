abde115e8b0105c7bddb2c89a4c1f79cff0f93dd
markettrade724-max <markettrade724@gmail.com>
2026-05-15 17:34:16 +0100

    Create Bullet.gd for bullet mechanics
    Added Bullet class to handle bullet behavior and destruction.

---
+++ godot-projects/genesis-3d/Bullet.gd
@@ -0,0 +1,12 @@
+extends RigidBody3D
+
+var damage = 25.0
+var lifetime = 2.0
+
+func _ready():
+    # تدمير الرصاصة بعد فترة
+    await get_tree().create_timer(lifetime).timeout
+    queue_free()
+
+func launch(velocity_vector: Vector3):
+    linear_velocity = velocity_vector
