f5fc4d9ef4513b4341f3a4fbdab8cab3c356ac3e
markettrade724-max <markettrade724@gmail.com>
2026-05-16 15:36:26 +0200

    Refactor input handling to include touch events

--- godot-projects/genesis-3d/main_scene.gd
+++ godot-projects/genesis-3d/main_scene.gd
@@ -5,10 +5,14 @@ var game_started = false
 func _ready():
     Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
     get_tree().paused = true
+    process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _input(event):
-    if not game_started and event is InputEventMouseButton and event.pressed:
-        start_game()
+    if not game_started:
+        var is_click = event is InputEventMouseButton and event.pressed
+        var is_touch = event is InputEventScreenTouch and event.pressed
+        if is_click or is_touch:
+            start_game()
 
 func start_game():
     game_started = true
