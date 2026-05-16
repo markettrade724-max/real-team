fa9eefffdf950fcb341fa6438a5f77c700bcd4ed
markettrade724-max <markettrade724@gmail.com>
2026-05-16 15:37:42 +0200

    Add collision handling for enemy damage in bullet

--- godot-projects/genesis-3d/bullet.gd
+++ godot-projects/genesis-3d/bullet.gd
@@ -9,3 +9,8 @@ func _ready():
 
 func launch(velocity_vector: Vector3):
     linear_velocity = velocity_vector
+
+func _on_body_entered(body: Node):
+    if body.is_in_group("enemy") and body.has_method("take_damage"):
+        body.take_damage(damage)
+    queue_free()
